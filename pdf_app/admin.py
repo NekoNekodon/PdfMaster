6713c4bff20ea7b6deff34de450defb08318abd7
NekoNekodon <nekodondayo@gmail.com>
2026-07-25 23:08:04 +0800

    Delete pdf_app directory

--- pdf_app/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import PdfTask
-
-@admin.register(PdfTask)
-class PdfTaskAdmin(admin.ModelAdmin):
-    list_display = ["id", "file_name", "start_page", "end_page", "status", "create_time"]
-    list_filter = ["status"]
-    search_fields = ["file_name"]
